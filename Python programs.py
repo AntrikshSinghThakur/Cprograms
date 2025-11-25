0177b256b274bbe59a8f6b0f31d1d839694c555c
AntrikshSinghThakur <thakurantrikshsingh50@gmail.com>
2025-11-25 21:15:08 +0530

    Delete Python programs.py

--- Python programs.py
+++
@@ -1,9 +0,0 @@
-num1 = int(input("Enter 1st number: "))
-num2 = int(input("Enter 2nd number:"))
-i = 1
-while(i<= num1 and i<= num2):
-    if(num1 and i<=  num2):
-        gcd = i
-     i = i + 1
-
-print("GCD is", gcd)
